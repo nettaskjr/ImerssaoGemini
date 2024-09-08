682619df732587afe5251cc545c82adc4e03a085
Nestor Junior <nestor.junior@gmail.com>
2024-09-08 18:23:29 -0300

    add apps.js and dados.js, modify index.html

---
+++ dados.js
@@ -0,0 +1,22 @@
+let dados = [
+    {
+        nome: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta brasileira que fez história ao se tornar a primeira brasileira a conquistar uma medalha de ouro em ginástica artística nos Jogos Olímpicos. Conhecida por suadeterminação e talento, ela é considerada um dos maiores nomes do esporte no Brasil.",
+        link: ""
+    },
+    {
+        nome: "Daiane dos Santos",
+        descricao: "Daiane dos Santos é uma ginasta brasileira conhecida por sua habilidade no salto. Foi a primeirabrasileira a conquistar uma medalha em uma final de aparelho em um Mundial e Ginástica Artística, no ano de 2003.",
+        link: ""
+    },
+    {
+        nome: "Flavia Saraiva",
+        descricao: "Flavia Saraiva é uma ginasta brasileira que se destacou por sua versatilidade e carisma. Conquistou diversas medalhas em competições internacionais e é considerada uma das maiores promessas da ginástica brasileira.",
+        link: ""
+    }
+];
+
+
+
+
+
