3e32fcb0fe8d5f7f5ed5c6342c6119668664cab4
Nestor Junior <nestor.junior@gmail.com>
2024-09-09 00:40:15 -0300

    update files

--- app.js
+++ app.js
@@ -1,1 +1,51 @@
-console.log (dados);
+function pesquisar() {
+    // Obtém a seção onde os resultados serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+
+    // Obtém o valor digitado pelo usuário no campo de pesquisa e converte para minúsculas
+    let campoPesquisa = document.getElementById("campo-pesquisa").value.toLowerCase();
+
+    // Verifica se o campo de pesquisa está vazio
+    if (!campoPesquisa) {
+        // Exibe uma mensagem de erro caso o campo esteja vazio
+        section.innerHTML = "<p>Campo vazio, favor digitar um dado para pesquisa</p>";
+        return; // Interrompe a função
+    }
+
+    // Inicializa variáveis para armazenar os resultados e os valores a serem comparados
+    let resultados = "";
+    let nome = "";
+    let descricao = "";
+    let tags = "";
+
+    // Itera sobre os dados e busca por correspondências
+    for (let dado of dados) {
+        nome = dado.nome.toLowerCase();
+        descricao = dado.descricao.toLowerCase();
+        tags = dado.tags.toLowerCase();
+
+        // Verifica se o termo de pesquisa está presente no nome, descrição ou tags
+        if (nome.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+            // Constrói o HTML para cada resultado encontrado
+            resultados += `
+                <div class="item-resultado">
+                    <h2>
+                        <a href="${dado.link}" target="_blank">${dado.nome}</a>
+                    </h2>
+                    <p class="descricao-meta">
+                        ${dado.descricao}
+                    </p>
+                </div>
+            `;
+        }
+    }
+
+    // Verifica se algum resultado foi encontrado
+    if (!resultados) {
+        // Exibe uma mensagem informando que nenhum resultado foi encontrado
+        resultados = "<p>Nenhum resultado foi encontrado</p>";
+    }
+
+    // Atualiza o conteúdo da seção de resultados com os resultados encontrados
+    section.innerHTML = resultados;
+}
--- dados.js
+++ dados.js
@@ -2,17 +2,20 @@ let dados = [
     {
         nome: "Rebeca Andrade",
         descricao: "Rebeca Andrade é uma ginasta brasileira que fez história ao se tornar a primeira brasileira a conquistar uma medalha de ouro em ginástica artística nos Jogos Olímpicos. Conhecida por suadeterminação e talento, ela é considerada um dos maiores nomes do esporte no Brasil.",
-        link: ""
+        link: "https://www.instagram.com/rebecarandrade/",
+        tags: "ginasta ginastica ginástica"
     },
     {
         nome: "Daiane dos Santos",
         descricao: "Daiane dos Santos é uma ginasta brasileira conhecida por sua habilidade no salto. Foi a primeirabrasileira a conquistar uma medalha em uma final de aparelho em um Mundial e Ginástica Artística, no ano de 2003.",
-        link: ""
+        link: "https://www.instagram.com/daiane_gs_/",
+        tags: "ginasta ginastica ginástica aparelho 2003"
     },
     {
         nome: "Flavia Saraiva",
         descricao: "Flavia Saraiva é uma ginasta brasileira que se destacou por sua versatilidade e carisma. Conquistou diversas medalhas em competições internacionais e é considerada uma das maiores promessas da ginástica brasileira.",
-        link: ""
+        link: "https://www.instagram.com/flavialopessaraiva/",
+        tags: "ginasta ginastica ginástica"
     }
 ];
 
